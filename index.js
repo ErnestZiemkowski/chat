cef8a556178c61d7586b29da07336714e9a8e5ca
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-17 01:31:18 +0200

    chat working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const port = process.env.port || 5000;
@@ -7,3 +8,18 @@ const server = app.listen(port, () => {
 });
 
 app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection ' + socket.id);
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,27 @@
+const socket = io.connect('http://localhost:5000');
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
+
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+});
+
+socket.on('chat', (data) => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>'+ data.message +'</p>';
+});
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p><em>' + data + 'is typing...</em></p>';
+});
