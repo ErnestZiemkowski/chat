541e905c1295c8f5208600ac2002fe136970695c
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-17 02:11:46 +0200

    hotfix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require('socket.io');
 const path = require('path');
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
     console.log('llistening on port: ' + port);
 });
